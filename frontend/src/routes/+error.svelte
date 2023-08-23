5d1f7c5b43f85744d00d6b27af902e7976a1fc06
DataHearth <antoine.l@antoine-langlois.net>
2023-08-23 16:23:54 +0200

    add error button a tag

--- frontend/src/routes/+error.svelte
+++ frontend/src/routes/+error.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
+	import { afterNavigate } from '$app/navigation';
+	import { base } from '$app/paths';
 	import { page } from '$app/stores';
 	import Icon from '@iconify/svelte';
+
+	let previousPage: string = base;
+
+	afterNavigate(({ from }) => {
+		previousPage = from?.url.pathname || previousPage;
+	});
 </script>
 
 <section class="bg-white dark:bg-gray-900">
@@ -18,19 +26,22 @@
 			</h1>
 
 			<div class="flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto">
-				<button
+				<a
+					href={previousPage}
+					rel="prefetch"
 					class="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700"
 				>
 					<Icon icon="radix-icons:pin-left" />
 					Go back
-				</button>
+				</a>
 
-				<button
+				<a
+					href="/"
 					class="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-white transition-colors duration-200 bg-blue-500 rounded-lg sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600"
 				>
 					<Icon icon="clarity:home-line" class="mr-2" />
 					Take me home
-				</button>
+				</a>
 			</div>
 		</div>
 	</div>
